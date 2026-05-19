e25e37613fcc66f3e217486285450971d7f302b3
Mazen Tarek <mazen.hanafy.ext@siemens.com>
2026-05-20 01:34:51 +0300

    use langchain-tavily

--- main.py
+++ main.py
@@ -3,25 +3,13 @@ from langchain_groq import ChatGroq
 from langchain.agents import create_agent
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
-tavily = TavilyClient()
 load_dotenv()
 
-@tool
-def search(query: str) -> str:
-    """
-        Tool that searches the internet
-        Args:
-            query: The query to search for
-        Returns:
-            The Search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
 
-llm = ChatGroq(temperature=0, model="openai/gpt-oss-120b")
-tools=[search]
+llm = ChatGroq(temperature=0, model="llama-3.3-70b-versatile")
+tools=[TavilySearch()]
 agent=create_agent(model=llm, tools=tools)
 
 def main():
